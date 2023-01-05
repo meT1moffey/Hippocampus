01687ab3d8949ac528175f5dab7e6854405bf49c
meT1moffey <96593529+meT1moffey@users.noreply.github.com>
2023-01-05 17:45:19 +0300

    Added coding funcion

--- Hippocampus/Services/Coder.cs
+++ Hippocampus/Services/Coder.cs
@@ -1,9 +1,19 @@
-﻿using System;
-
-namespace Hippocampus.Services
+﻿namespace Hippocampus.Services
 {
     public class Coder
     {
         public Coder() { }
+
+        public string Code(string data, string key)
+        {
+            string coded = "";
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                coded += data[i] ^ key[i % key.Length];
+            }
+
+            return coded;
+        }
     }
 }
