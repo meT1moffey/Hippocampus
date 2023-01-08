29f0b612b3e5a78d73e71426a972ceef4d7b2d6a
meT1moffey <96593529+meT1moffey@users.noreply.github.com>
2023-01-09 00:10:53 +0300

    Refactored class 'ImageVM'

--- Hippocampus/ViewModels/ImageWindowViewModel.cs
+++ Hippocampus/ViewModels/ImageWindowViewModel.cs
@@ -1,46 +1,22 @@
-﻿using Hippocampus.Services;
-using ReactiveUI;
+﻿using ReactiveUI;
 using Avalonia.Media.Imaging;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace Hippocampus.ViewModels
 {
     public class ImageWindowViewModel : ViewModelBase
     {
-        string source = "C:\\Tima\\Scripti\\Hippocampus\\Hippocampus\\TestImage.png";
         Bitmap? image;
-        int width, height;
-
-        public int Width
-        {
-            get => width;
-            private set => this.RaiseAndSetIfChanged(ref width, value);
-        }
-
-        public int Height
-        {
-            get => height;
-            private set => this.RaiseAndSetIfChanged(ref height, value);
-        }
 
         public Bitmap? Image
         {
             get => image;
             private set => this.RaiseAndSetIfChanged(ref image, value);
         }
-        public string Source
-        {
-            get => source;
-            set => this.RaiseAndSetIfChanged(ref source, value);
-        }
 
         public ImageWindowViewModel(Stream image)
         {
             Image = Bitmap.DecodeToHeight(image, 1000);
-
-            Width = Image.PixelSize.Width;
-            Height = Image.PixelSize.Height;
         }
     }
 }
--- Hippocampus/ViewModels/OutputOptionViewModel.cs
+++ Hippocampus/ViewModels/OutputOptionViewModel.cs
@@ -1,26 +1,18 @@
-﻿using ReactiveUI;
-using System.Reactive;
-using Hippocampus.Services;
-using System;
-using System.IO;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using Avalonia.Controls;
-using Hippocampus.Models;
+﻿using Hippocampus.Models;
 
 namespace Hippocampus.ViewModels
 {
     public class OutputOptionViewModel : ViewModelBase
     {
         OutputOption option;
+        public string OptionName
+        {
+            get => option.option.ToString();
+        }
 
         public OutputOptionViewModel(OutputOption _option) => option = _option;
 
         public OutputOptionEnum GetOption() => option.option;
 
-        public string OptionName
-        {
-            get => option.option.ToString();
-        }
     }
 }
